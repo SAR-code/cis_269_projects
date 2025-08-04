04e60fb57c44a9c35a66268c000973abdd97337e
SAR-code <dylanmccallum28@gmail.com>
2025-08-03 19:24:50 -0700

    Assignment 1 complete

---
+++ week_9/BinaryTree/BinaryTree/BinaryTreeType.cpp
@@ -0,0 +1,67 @@
+/*
+* script: BinaryTreeType.cpp
+* action: This program contains the implementation
+*         of the BinaryTreeType file.
+* author: D.M
+* date: 03AUG2025
+*/
+
+#include "binaryTreeType.h"
+#include <iostream>
+
+using namespace std;
+
+// Constructor
+binaryTreeType::binaryTreeType() {
+    root = nullptr;
+}
+
+// Destructor
+binaryTreeType::~binaryTreeType() {
+    destroy(root);
+}
+
+// Method function to destroy the tree nodes
+void binaryTreeType::destroy(TreeNode* node) {
+    if (node != nullptr) {
+        destroy(node->left);
+        destroy(node->right);
+        delete node;
+    }
+}
+
+// Method function to insert a value into the tree
+void binaryTreeType::insert(int value) {
+    insertTreeNode(root, value);
+}
+
+// Method function to count the number of nodes in the tree
+int binaryTreeType::nodeCount() {
+    return countNodes(root);
+}
+
+// Member function to insert a new node in the tree
+void binaryTreeType::insertTreeNode(TreeNode*& node, int value) {
+    if (node == nullptr) {
+        // Create new node if current position is empty
+        node = new TreeNode{ value, nullptr, nullptr };
+    }
+    else if (value < node->data) {
+        // Recursion on left subtree
+        insertTreeNode(node->left, value);   
+    }
+    else {
+        // Recursion on right subtree
+        insertTreeNode(node->right, value);  
+    }
+}
+
+// Member function to count the number of nodes in the tree
+int binaryTreeType::countNodes(TreeNode* node) {
+    if (node == nullptr)
+        // Return 0 if base case
+        return 0;
+    else
+        // Count current node + nodes in left + nodes in right
+        return 1 + countNodes(node->left) + countNodes(node->right);
+}
